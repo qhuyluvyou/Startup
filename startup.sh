63920ba9e5bf1bfa431db7c775411c4536965b77
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-14 06:54:43 +0700

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,1 @@
+sudo apt update && sudo apt upgrade -y && sudo apt install ufw screen openjdk-21-jdk && curl -fsSL https://tailscale.com/install.sh | sh && sudo systemctl unmask tailscaled && sudo systemctl start tailscaled && sudo tailscale up --auth-key=tskey-auth-kRUzmudqjH11CNTRL-SF627ePitxBaoKqAQLxcyBU9XS9ejQ9d && cd Server && qhuy
