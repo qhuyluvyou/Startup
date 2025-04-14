b0abd91a7092576b830096f341e2c7272222bfcf
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-14 11:02:20 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ sudo apt install -y ufw screen openjdk-21-jdk && \
 curl -fsSL https://tailscale.com/install.sh | sh && \
 sudo systemctl unmask tailscaled && \
 sudo systemctl start tailscaled && \
-sudo tailscale up --auth-key=tskey-auth-kRUzmudqjH11CNTRL-SF627ePitxBaoKqAQLxcyBU9XS9ejQ9d && \
+sudo tailscale up --auth-key=tskey-auth-kXJp9mDbTC11CNTRL-hyMFJM66zwVuHPmD7ZjRwVg78bjUiYAk && \
 cd Server && qhuy
