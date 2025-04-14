4cb4bf31f50442035da9c9f9154d25e51b7405ba
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-14 17:52:26 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,15 @@
-apt update && sudo apt upgrade -y && \
-apt install -y ufw screen openjdk-21-jdk xdotool && \
+sudo apt update && sudo apt upgrade -y && \
+sudo apt install -y ufw screen openjdk-21-jdk xdotool && \
 curl -fsSL https://tailscale.com/install.sh | sh && \
-systemctl unmask tailscaled && \
-systemctl start tailscaled && \
-tailscale up --auth-key=tskey-auth-kFjQJoDX6D21CNTRL-wtLGQLWew7CVTxogc7PH7C6X5hJgdkHw && \
+sudo systemctl unmask tailscaled && \
+sudo systemctl start tailscaled && \
+sudo tailscale up --auth-key=tskey-auth-kYNEh9t4t111CNTRL-C6H1UGZBvZ3njdkFMxYMZ3Zw1HEjtLFX
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing' && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0 && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing' && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0 && \
 gsettings set org.gnome.desktop.session idle-delay 0 && \
-ln -sf /dev/null /sbin/shutdown
-ln -sf /dev/null /sbin/poweroff
-ln -sf /dev/null /sbin/reboot
-curl -o xdotool.sh https://raw.githubusercontent.com/qhuyluvyou/Startup/refs/heads/main/Xdotool.sh
-chmod +x xdotool.sh
-screen -r loop
+sudo ln -sf /dev/null /sbin/shutdown
+sudo ln -sf /dev/null /sbin/poweroff
+sudo ln -sf /dev/null /sbin/reboot
+
