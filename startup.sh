d29ab0901210e25a926dbf4f362e1744a2b660e3
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-15 11:35:00 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 sudo apt update && sudo apt upgrade -y && \
 sudo apt install -y ufw screen openjdk-21-jdk xdotool && \
-curl -fsSL https://tailscale.com/install.sh | sh && \
-sudo systemctl unmask tailscaled && \
-sudo systemctl start tailscaled && \
-sudo tailscale up --auth-key=tskey-auth-kYNEh9t4t111CNTRL-C6H1UGZBvZ3njdkFMxYMZ3Zw1HEjtLFX
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing' && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0 && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing' && \
