5180eb1a62f82baa041e2ba32599b9bdaf36ece2
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-16 20:48:26 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 sudo apt update && sudo apt upgrade -y && \
 sudo apt install -y ufw screen openjdk-21-jdk xdotool && \
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kJh3pY17ue11CNTRL-AiZx4NvPjo7nEjGmhHcEo72yKjWYoNK1
+curl -fsSL https://tailscale.com/install.sh | sh && \
 sudo systemctl unmask tailscaled && sudo systemctl start tailscaled && \
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kJh3pY17ue11CNTRL-AiZx4NvPjo7nEjGmhHcEo72yKjWYoNK1 --reset
+sudo tailscale up --login-server https://early-certain-walrus.ngrok-free.app --auth-key=f4381c71bedf549e20eb06d176cf368cfb4a39a28d3814c0 && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing' && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0 && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing' && \
@@ -11,4 +11,3 @@ gsettings set org.gnome.desktop.session idle-delay 0 && \
 sudo ln -sf /dev/null /sbin/shutdown
 sudo ln -sf /dev/null /sbin/poweroff
 sudo ln -sf /dev/null /sbin/reboot
-
