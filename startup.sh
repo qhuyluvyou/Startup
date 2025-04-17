d6a1c59f5960947bd140c8c6deee06e8dd31334f
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-17 12:08:00 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,7 @@ gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing' && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0 && \
 gsettings set org.gnome.desktop.session idle-delay 0 && \
-sudo ln -sf /dev/null /sbin/shutdown
-sudo ln -sf /dev/null /sbin/poweroff
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target && \
+sudo ln -sf /dev/null /sbin/shutdown && \
+sudo ln -sf /dev/null /sbin/poweroff && \
 sudo ln -sf /dev/null /sbin/reboot
