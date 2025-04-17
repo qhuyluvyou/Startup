b3ec04b8c5944a0b27dddd8389a5c731b7d9e870
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-17 20:16:44 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade -y && \
 sudo apt install -y ufw screen openjdk-21-jdk xdotool && \
 curl -fsSL https://tailscale.com/install.sh | sh && \
 sudo systemctl unmask tailscaled && sudo systemctl start tailscaled && \
-sudo tailscale up --login-server https://early-certain-walrus.ngrok-free.app --auth-key=f4381c71bedf549e20eb06d176cf368cfb4a39a28d3814c0 && \
+sudo tailscale up --login-server https://early-certain-walrus.ngrok-free.app --auth-key=5087899917683a91beb627cd83936bde6f7a174f914759bb && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing' && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0 && \
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing' && \
