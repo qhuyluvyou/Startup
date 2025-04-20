c392af03feea0e644881d4ebbd35cea81433ff46
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-20 10:52:26 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -25,7 +25,7 @@ cp paper-1.19.4-550.jar paper.jar
 source ~/.bashrc
 
 cd ~/Hyr
-wget https://drive.google.com/uc?export=download&id=1WvkNAQ6n5KABzcqan0IJ5oQ_CPgLOzcT
+wget https://www.dropbox.com/scl/fi/3wvmismquwljlvstqq923/HYRONIC_SURVIVAL_v2.1.11.zip?rlkey=41ksjp28vpm50uixd6iclqffy&st=japl8g0g&dl=1
 
 unzip HYR*.zip
 cd Hyr*
