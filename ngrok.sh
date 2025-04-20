1586661c720d0d551e6b56f081b55a02f1f42f89
qhuyluvyou <digitalzisthebest@gmail.com>
2025-04-20 09:54:47 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
   && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
